cb72855ab96c23008958ca8c29b7f036d8abde82
eddie <e.jamesduro@gmail.com>
2016-01-05 20:40:38 -0800

    updated carousel imgs, and arrows

--- public/js/script.js
+++ public/js/script.js
@@ -6,8 +6,22 @@ $(document).ready(function(){
             $('#scroll').fadeOut(); 
         } 
     }); 
+
     $('#scroll').click(function(){ 
         $("html, body").animate({ scrollTop: 0 }, 600); 
         return false; 
     }); 
+
+});
+
+ // carousel 
+$(document).ready(function(){
+    $('.strains').slick({
+    	infinite: true,
+  		slidesToShow: 3,
+		slidesToScroll: 1,
+		arrows: true,
+		prevArrow: $('.left-arrow'),
+        nextArrow: $('.right-arrow'),
+	});
 });
